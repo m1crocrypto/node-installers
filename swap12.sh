239c764bfa50ff2be5ced2cde472f7adc769347b
_Maxx <93737616+m1crocrypto@users.noreply.github.com>
2023-06-23 09:14:06 +0400

    Create swap12.sh

---
+++ swap12.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd $HOME
+
+sudo swapon --show
+
+# Отключаем текущий свап
+sudo swapoff -a
+
+# Добавляем своп на 12 гигов
+sudo fallocate -l 12G /swapfile
+
+# Show storage
+ls -lh /swapfile
+
+# Активация swap
+sudo chmod 600 /swapfile
+ls -lh /swapfile
+
+# Создание файловой системы свап
+sudo mkswap /swapfile
+
+# Включаем свап
+sudo swapon /swapfile
+
+# Убеждаемся что он включен
+swapon --show
+
+# Добавляем свап как постоянный
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
